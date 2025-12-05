c8a6771906e97f34c5e6ea325e93e1b04d5f61d2
faziolifabrizio-jpg <fazioli.fabrizio@gmail.com>
2025-12-05 15:45:02 +0100

    Update server.py

--- server.py
+++ server.py
@@ -1,14 +1,27 @@
 from http.server import BaseHTTPRequestHandler, HTTPServer
+import subprocess
 
 class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        self.send_response(200)
+    def _send(self, code, msg):
+        self.send_response(code)
         self.send_header("Content-type", "text/plain")
         self.end_headers()
-        self.wfile.write(b"Bot Amazon attivo (Render FREE)")
+        self.wfile.write(msg.encode())
+
+    def do_GET(self):
+        if self.path == "/":
+            self._send(200, "OK - Amazon Bot Online.")
+        elif self.path == "/run":
+            self._send(200, "Esecuzione avviata.")
+            subprocess.Popen(["python", "bot.py"])
+        else:
+            self._send(404, "Not Found")
+
+def start():
+    PORT = 10000
+    server = HTTPServer(("", PORT), Handler)
+    print(f"[SERVER] Attivo sulla porta {PORT}")
+    server.serve_forever()
 
 if __name__ == "__main__":
-    port = 10000
-    server = HTTPServer(("", port), Handler)
-    print(f"Server attivo su porta {port}")
-    server.serve_forever()
+    start()
